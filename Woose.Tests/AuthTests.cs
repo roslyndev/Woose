632f2b92e277c79cd497d246821c0bfad7bc2cb8
parkheesung <parkheesung@eyemcast.com>
2023-12-17 14:33:45 +0900

    인증토큰 테스트

--- Woose.Tests/AuthTests.cs
+++ Woose.Tests/AuthTests.cs
@@ -15,7 +15,7 @@ namespace Woose.Tests
         public void CreateJWT()
         {
             CryptoHandler crypto = new CryptoHandler();
-            JwtToken? token = crypto.GenerateTokens("yunlang97@naver.com", "¿¬¶û", "test-server-token");
+            JwtToken? token = crypto.GenerateTokens("userid", "webmaster", "test-server-token");
 
             Assert.IsNotNull(token);
         }
